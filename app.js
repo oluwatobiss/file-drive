876b9af0e39e0cd7d10227c70fc1aacefcfa9cac
Oluwatobi Sofela <60105594+oluwatobiss@users.noreply.github.com>
2024-10-17 16:27:30 +0100

    style: Design homepage

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const path = require("node:path");
+
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello there"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => console.log("Server listening at port 3000"));
+app.use((req, res, next) => {
+  res.locals.appName = "File Drive";
+  next();
+});
+
+app.get("/", (req, res) => res.render("index"));
+
+app.use((err, req, res, next) => {
+  const userData = req.user;
+  err &&
+    res.status(400).render("error", {
+      userStatus: userData?.status,
+      error: `${err.message}`,
+    });
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!`));
