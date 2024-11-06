efe03d27c21b2c49b4e039078b92ff4a947a83a4
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2024-11-06 20:04:44 +0100

    feat: Create modal to rename folders

--- public/index.js
+++ public/index.js
@@ -5,6 +5,13 @@ function openCreateFolderModal(popup) {
     : (createFolderModal.style.display = "");
 }
 
+function openRenameFolderModal(popup) {
+  const renameFolderModal = document.getElementById("rename-folder-modal");
+  popup
+    ? (renameFolderModal.style.display = "flex")
+    : (renameFolderModal.style.display = "");
+}
+
 function openRowOptionsModal(event) {
   const rowOptionsModal =
     event.currentTarget.parentElement.querySelector(".row-options-modal");
